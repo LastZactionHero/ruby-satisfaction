d2b18a69ff747f9aaa9048fd31f2545df0111152
Zach Dicklin <zdicklin@gmail.com>
2011-08-30 10:13:08 -0600

    Updated gemspec

--- init.rb
+++ init.rb
@@ -1,1 +1,17 @@
 require 'satisfaction'
+
+  # Create a Get Satisfaction Topic page for a new topic
+       
+    sfn = Satisfaction.new
+    sfn.set_basic_auth("michael@crowdspoke.com", "1lucidxyz")
+    #sfn.set_consumer('token goes here', 'secret goes here')
+    #response = sfn.people['jargon'].topics.post(
+    response = sfn.topics.post(
+      :company_domain => 'Crowdspoke',
+      :style => 'idea',
+      :subject => "Topic Management: Topic Name From Command Line",
+      #:additional_detail => "I seem to be made of metal.",
+      :keywords => nil
+    )
+    puts response.inspect
+    
--- ruby-satisfaction.gemspec
+++ ruby-satisfaction.gemspec
@@ -24,7 +24,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency "gemcutter", ">= 0.6.1"
   s.add_development_dependency "rspec", "~> 2.2"
   s.add_development_dependency "fakeweb", "~> 1.3"
-  s.add_development_dependency "open_gem", ">= 1.4"
+  #s.add_development_dependency "open_gem", ">= 1.4"
   #s.add_development_dependency "ruby-debug", ">= 0.10.4"
   s.add_development_dependency "rr", '1.0.2'
 
